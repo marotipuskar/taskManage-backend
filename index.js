9e3cf794a922f6823899e6e63fc733564b8201a4
Maroti Puskar <119912274+marotipuskar@users.noreply.github.com>
2025-01-18 15:25:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // https://frontend-task-management-wine.vercel.app/
 // app.use(cors());                 // Allows cross-origin requests from frontend
-app.use(cors({ origin: 'https://frontend-task-management-wine.vercel.app' }));
+app.use(cors({ origin: 'https://frontend-task-management-wine.vercel.app',
+             methods: ['GET', 'POST', 'PUT', 'DELETE'],
+             }));
 
 // app.use(cors({
 //   origin: "https://frontend-task-management-wine.vercel.app", // Allow your frontend domain
